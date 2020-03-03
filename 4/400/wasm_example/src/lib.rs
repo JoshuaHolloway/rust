1e1d2808127cc6a3a471763ab7221d4aa6bcc826
JoshuaHolloway <jhollow6@asu.edu>
2020-03-03 14:58:39 -0600

    Applied wasm-bindgen to the new web-assembly file

--- 4/400/wasm_example/src/lib.rs
+++ 4/400/wasm_example/src/lib.rs
@@ -1,9 +1,47 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+
+    type HTMLDocument;
+    type Element;
+
+    static document: HTMLDocument;
+
+    #[wasm_bindgen(method)]
+    fn createElement(this: &HTMLDocument, tagName: &str) -> Element;
+
+    #[wasm_bindgen(method, getter)]
+    fn body(this: &HTMLDocument) -> Element;
+
+    #[wasm_bindgen(method, js_name = appendChild)]
+    fn append(this: &Element, item: Element);
+
+    #[wasm_bindgen(method, setter = innerHTML)]
+    fn set_inner(this: &Element, html: &str);
+}
+
+#[wasm_bindgen]
+pub fn run_alert(item: &str) {
+    alert(&format!("This is WASM and {}", item));
+}
+
+#[wasm_bindgen]
+pub fn create_stuff() {
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+
+    p.set_inner("Hello from WASM in Rust!");
+    div.append(p);
+
+    document.body().append(div);
 }
 
 // > cargo build
+// > cargo build --target wasm32-unknown-unknown
+
+// Apply wasm-bindgen to the new webassembly file
+// > wasm-bindgen target/wasm32-unknown-unknown/debug/wasm_example.wasm --out-dir .
